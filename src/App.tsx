07e0f8a44e78ebfeff292f523274825922a822a8
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 11:52:43 +0200

    add ProgressSteps component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import family from "./assets/family.svg";
 import CardComponentProps from "./components/CardComponent";
+import ProgressSteps from "./components/ProgressSteps";
 
 function App() {
   return (
     <main className="flex justify-center items-center h-screen">
+      <div className="flex flex-col gap-10">
       <CardComponentProps image={family} alt="Family" position="down" title="Baby's birth" />
+      <ProgressSteps/>
+      </div>
     </main>
   );
 }
