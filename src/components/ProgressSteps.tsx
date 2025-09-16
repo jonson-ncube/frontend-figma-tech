b6c058fc83296b444d46ebe53bf7cea290476f7c
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 09:14:26 +0200

    wip-progress steps

---
+++ src/components/ProgressSteps.tsx
@@ -0,0 +1,48 @@
+export default function ProgressSteps() {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex items-center justify-center w-[18px] h-[18px] rounded-full bg-[#00B897]">
+        <div className="w-[8px] h-[8px] rounded-full bg-[#215A6B]" />
+      </div>
+      <div className="border-t-2 h-0 w-[65px] rounded-lg" />
+      <div className="w-[8px] h-[8px] rounded-full bg-[#00B897]" />
+      <div className="border-t-2 h-0 w-[65px] rounded-lg" />
+      <div className="w-[8px] h-[8px] rounded-full bg-[#00B897]" />
+      <div className="border-t-2 h-0 w-[65px] rounded-lg" />
+      <div className="w-[8px] h-[8px] rounded-full bg-[#00B897]" />
+      <div className="border-t-2 h-0 w-[65px] rounded-lg" />
+      <div className="w-[8px] h-[8px] rounded-full bg-[#00B897]" />
+      <svg
+        width="140"
+        height="3"
+        viewBox="0 0 140 3"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1.5L139 1.49999"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-dasharray="1 7"
+        />
+      </svg>
+
+      <svg
+        width="12"
+        height="20"
+        viewBox="0 0 12 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.48797 1.95312C3.92216 4.32595 9.1251 9.206 10.4634 9.74367C7.53293 11.9538 1.53766 16.7087 1 18.047"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
