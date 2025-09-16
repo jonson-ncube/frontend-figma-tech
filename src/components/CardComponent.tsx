734aceb1a1fb2409b040b938c148553cf65bcadb
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 16:39:21 +0200

    fix: adjust CardComponent styles for better layout

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -9,9 +9,9 @@ type CardComponentProps = {
 
 export default function CardComponent({image, alt, title, position = "down"} : CardComponentProps) {
   return (
-    <div className={"flex flex-col relative items-center bg-[#173F4B] w-[130px] h-[136px] rounded-[5px] p-2 shadow-[0px_4px_12px_rgba(0,0,0,0.25)]"}>
+    <div className={"flex flex-col relative items-center bg-[#173F4B] w-[130px] min-h-[136px] rounded-[5px] p-2 shadow-[0px_4px_12px_rgba(0,0,0,0.25)]"}>
       <img className="h-[76px] w-[114px] rounded" src={image} alt={alt} />
-      <p className={"font-normal text-sm py-3"}>{title}</p>
+      <p className={"font-normal text-center text-sm py-3"}>{title}</p>
       <TrianglePointer position={position}/>
     </div>
   );
