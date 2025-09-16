6464c7b2f1ab39e95e3cb93e7198764d4ad7ec7d
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 20:12:53 +0200

    updated: remove unnecessary whitespace in goals labels

--- src/data/goals.ts
+++ src/data/goals.ts
@@ -1,8 +1,10 @@
 export const goals=[
-    { label: "You are \n here" },
-    { label: "In 1 year and 9 \n months" },
-    { label: "In 3 years and \n 2 months" },
-    { label: "In 4 years and \n 9 months" },
-    { label: "In 8 years and \n 11 months" },
+    { label: "You are\nhere" },
+    { label: "In 1 year and 9\nmonths" },
+    { label: "In 3 years and\n2 months" },
+    { label: "In 4 years and\n9 months" },
+    { label: "" },
+    { label: "In 8 years and\n11 months" },
+    { label: "" },
     { label: "Ultimately" },
   ];
