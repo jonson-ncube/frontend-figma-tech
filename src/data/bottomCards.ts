59e8e910a0cd56c214004f3435c8ed7e12e76a41
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 15:55:16 +0200

    add bottomCards and topCards data files with card details

---
+++ src/data/bottomCards.ts
@@ -0,0 +1,31 @@
+import home from "../assets/home.svg";
+import efunds from "../assets/em-fund.svg";
+import debt from "../assets/debt.svg";
+import community from "../assets/family.svg";
+
+export const bottomCarts = [
+    {
+    image: home,
+    alt: "Home",
+    position: "up",
+    title: "Buy a home",
+  },
+  {
+    image: efunds,
+    alt: "Emergency Funds",
+    position: "up",
+    title: "Emergency Funds",
+  },
+  {
+    image: debt,
+    alt: "debt-free",
+    position: "up",
+    title: "Debt free",
+  },
+  {
+      image: community,
+      alt: "community",
+      position: "up",
+      title: "make a contribution to my community through philanthropy",
+    },
+];
---
+++ src/data/topCards.ts
@@ -0,0 +1,23 @@
+import family from "../assets/family.svg";
+import holiday from "../assets/going-holiday.svg";
+
+export const topCards = [
+  {
+    image: family,
+    alt: "Family",
+    position: "down",
+    title: "Baby's birth",
+  },
+  {
+    image: holiday,
+    alt: "Vacation",
+    position: "down",
+    title: "Holiday",
+  },
+  {
+    image: family,
+    alt: "retirement",
+    position: "down",
+    title: "Retire",
+  },
+];
