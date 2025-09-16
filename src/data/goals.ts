04a0a0dc0204034f1e1595f5a5163ed0a755c4ed
jonson-ncube <jonson.ncube@gmail.com>
2025-09-16 16:31:50 +0200

    add goals data file

---
+++ src/data/goals.ts
@@ -0,0 +1,8 @@
+export const goals=[
+    { label: "You are \n here" },
+    { label: "In 1 year and 9 \n months" },
+    { label: "In 3 years and \n 2 months" },
+    { label: "In 4 years and \n 9 months" },
+    { label: "In 8 years and \n 11 months" },
+    { label: "Ultimately" },
+  ];
